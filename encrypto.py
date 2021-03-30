b9a95304b0ab2cd661d27ad06d84c436dccca6fa
donkerevader <ctweeduizendvier@yahoo.com>
2021-03-30 10:50:33 +0200

    PEP8git status && Some refactoring && get rid of all those comments u don't need && file rename

--- encrypto.py
+++ encrypto.py
@@ -1,8 +1,10 @@
 import numpy as np
 import more_itertools
 
-__author__ = "Lukaeaeap"
+
+__authors__ = ["Lukaeaeap", "Donkere-vader"]
 __version__ = "0.1"
+
 
 KEY = np.matrix([
     [4, 3, 6, 9],
@@ -10,11 +12,13 @@ KEY = np.matrix([
     [6, 9, 7, 2],
     [2, -1, -9, 8]])
 
-def prin(print_label, printable_thing):
+
+def print_w_label(print_label, printable_thing):
     print(f"{print_label}:" )
     print(printable_thing)
 
-class Encrypt:
+
+class Encryptor:
     def __init__(self, message, key, ence_array, matrix_to_decrypt, encrypted_list):
         self.message = message
         self.key = key 
@@ -22,53 +26,38 @@ class Encrypt:
         self.matrix_to_decrypt = matrix_to_decrypt
         self.encrypted_list = encrypted_list
 
-    def encryption(self, message, key,matrix_to_decrypt,ence_array, encrypted_list):
+    def encrypt(self, message, key,matrix_to_decrypt,ence_array, encrypted_list):
         message_mat = list([ord(char) for char in self.message])
-        #prin("Message matrix",message_mat)
         output = [list(group) for group in more_itertools.grouper(message_mat, 4, 0)]  # [[1, 2, 3, 4], [5, 6, 0, 0]]
-        #prin("Grouped matrix",output)
         out = np.rot90(np.matrix(output), k=1)
-        #prin("Text matrix", out)
         self.matrix_to_decrypt = self.key*out
-        #prin("Encrypted matrix", oit)
         turnedenc = np.rot90(np.matrix(self.matrix_to_decrypt), k=-1)
-        #prin("Turned back", turned)
         enc_carray = np.rint(turnedenc)
-        #prin("Rounded",dec_carray)
         enc_list =  enc_carray.astype(int)
-        #prin("As integer ist",dec_list)
         str_list = enc_list.reshape(-1)
-        #prin("Straight line",done)
         self.ence_array = np.squeeze(np.asarray(str_list))
-        #prin("Encrypted list",ence_array)
+
         """===| List with encrypted characters |==="""
         self.encrypted_list = ' '.join(map(str, self.ence_array))
 
-class Decrypt:
+
+class Decryptor:
     def __init__(self, key, decrypted_message):
         self.key = key
         self.decrypted_message = decrypted_message
     
-    def decryption(self, inpu, key, decrypted_message):
-        prin("Input matrix",inpu.matrix_to_decrypt)
+    def decrypt(self, inpu, key, decrypted_message):
+        print_w_label("Input matrix",inpu.matrix_to_decrypt)
         dec_mat = np.linalg.inv(key) * inpu.matrix_to_decrypt
-        #prin("Decrypted matrix",dec_mat)
         turned = np.rot90(np.matrix(dec_mat), k=-1)
-        #prin("Turned back", turned)
         dec_carray = np.rint(turned)
-        #prin("Rounded",dec_carray)
         dec_list =  dec_carray.astype(int)
-        #prin("As integer ist",dec_list)
         done = dec_list.reshape(-1)
-        #prin("Straight line",done)
         dece_array = np.squeeze(np.asarray(done))
-        #prin("Decrypted list",dece_array)
 
-        # Translate the characters of the decrypted list in to a message, transform the list in to a steady string
         decrypted_mes = [chr(num) for num in dece_array]
         self.decrypted_message = ''.join(map(str, decrypted_mes))
-        #return decrypted_message
-        #prin("Output",decrypted_message)
+
 
 class Inpute:
     def __init__(self, encryptable_txt, show_list, decryptable_msg):
@@ -85,14 +74,15 @@ class Inpute:
 
     def after(self, show_list, encryptin):
         if self.show_list in ['yes', 'y']:
-            prin("Encrypted list",encryptin.encrypted_list)
-        prin("Encrypted matrix", encryptin.matrix_to_decrypt)
+            print_w_label("Encrypted list",encryptin.encrypted_list)
+        print_w_label("Encrypted matrix", encryptin.matrix_to_decrypt)
 
     def getdec(self, decryptable_msg):
         self.decryptable_msg = input("What is the code that needs to be decrypted?: ")
     
     def afterdec(self, decryptin):
-        prin("Decrypted message", decryptin.decrypted_message)
+        print_w_label("Decrypted message", decryptin.decrypted_message)
+
 
 def _main():  # _ at start of name to hint that this should be treated as a private function
     key = KEY  # something
@@ -102,23 +92,19 @@ def _main():  # _ at start of name to hint that this should be treated as a priv
 
     input_thing = Inpute(enc_txt, show_list, decbl_msg)
     input_thing.get(enc_txt, show_list)
-    encrypting = Encrypt(input_thing.encryptable_txt, key, None, None, None)
-    encrypting.encryption(input_thing.encryptable_txt, key, None, None, None)
-    #print(encrypting.encrypted_list)
-    #print(show_list)
-    #print(encrypting.encrypted_list)
+    encrypting = Encryptor(input_thing.encryptable_txt, key, None, None, None)
+    encrypting.encrypt(input_thing.encryptable_txt, key, None, None, None)
     input_thing.after(show_list, encrypting)
-    #input_thing.getdec(None)
-    #print(encrypting.matrix_to_decrypt)
     
-    decrypting = Decrypt(key, None)
-    decrypting.decryption(encrypting, key, None)
-    #With input:
+    decrypting = Decryptor(key, None)
+    decrypting.decrypt(encrypting, key, None)
+    # With input:
     """
     decrypting = Decrypt(key, None)
     decrypting.decryption(input_thing, key, None)
     """
     input_thing.afterdec(decrypting)
+
 
 if __name__ == "__main__":
     _main()
